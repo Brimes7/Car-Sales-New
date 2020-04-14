422254750fb24b24aeefbf461f21d309aed4f8b0
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-14 16:02:18 -0500

    Added Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import{carSaleReducer} from './reducers/reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carSaleReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+    ReactDOM.render(
+        //needs to be passed similar to props
+        <Provider store = {store}>
+            <App />   
+        </Provider>,
+    rootElement);
+
+
+
